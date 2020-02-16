ca833b87bc73243616609f2afef20b83f96dce75
Valentin Hulot <siriusval@hotmail.fr>
2020-02-16 22:26:36 +0200

    Sign in and up APIs working, to do next clean code and redirect after login

--- src/components/Nav/LoginModal/Content/SignInContent.js
+++ src/components/Nav/LoginModal/Content/SignInContent.js
@@ -37,40 +37,59 @@ export class SignInContent extends Component {
   }
 
   submit(model) {
+    //MODEL SENT
+    console.log('REQUEST: MODEL SENT:')
+    console.log(model)
     apis.auth
-      .localLogin(JSON.stringify(model))
+      .localLogin(model)
       .then(res => {
-        if (res.status == HttpStatus.OK) {
+        // SUCCES
+        console.log('SUCCES')
+        console.log(res)
+
+        if (res.status === HttpStatus.OK) {
           NotificationManager.success(
             'Nice to see you again!',
             'Success',
           )
-          this.props.history.push('/')
+          //this.props.history.push('/') //TODO, to fix, throws an error
         }
       })
       .catch(error => {
         if (error.response) {
+          // ERROR: SERVER RESPONSE
+          console.log('ERROR: SERVER RESPONSE')
+
+          const data = error.response.data
+          const status = error.response.status
+
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
-          console.log(error.response.data)
-          console.log(error.response.status)
-          console.log(error.response.headers)
+          console.log('data: ', data)
+          console.log('status: ', status)
+          console.log('headers: ', error.response.headers)
 
-          if (error.response.status) {
-            NotificationManager.error(
-              'Email and/or password incorrect',
-              'Error',
-            )
-          }
+          NotificationManager.error(
+            'Email and/or password incorrect',
+            'Error',
+          )
         } else if (error.request) {
+          // ERROR: SERVER NO RESPONSE
+          console.log('ERROR: SERVER NO RESPONSE')
+
           // The request was made but no response was received
           // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
           // http.ClientRequest in node.js
           console.log(error.request)
         } else {
+          // ERROR: REQUEST ERROR
+          console.log('ERROR: REQUEST ERROR')
+
           // Something happened in setting up the request that triggered an Error
           console.log('Error', error.message)
         }
+        //ERROR: CONFIG
+        console.log('ERROR: CONFIG')
         console.log(error.config)
       })
   }
--- src/components/Nav/LoginModal/Content/SignUpContent.js
+++ src/components/Nav/LoginModal/Content/SignUpContent.js
@@ -36,47 +36,60 @@ export class SignUpContent extends Component {
   }
 
   submit(model) {
+    //MODEL SENT
+    console.log('REQUEST: MODEL SENT:')
     console.log(model)
     apis.auth
       .localRegister(model)
       .then(res => {
+        // SUCCES
+        console.log('SUCCES')
         console.log(res)
         if (
-          res.status == HttpStatus.OK ||
-          res.status == HttpStatus.CREATED
+          res.status === HttpStatus.OK ||
+          res.status === HttpStatus.CREATED
         ) {
           NotificationManager.success(
             'Sucessfully registered!',
             'Success',
           )
-          this.props.history.push('/')
+          //this.props.history.push('/') //TODO : fix,throws an error
         }
       })
       .catch(error => {
         if (error.response) {
+          // ERROR: SERVER RESPONSE
+          console.log('ERROR: SERVER RESPONSE')
+
+          const data = error.response.data
+          const status = error.response.status
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
-          console.log(error.response.data)
-          console.log(error.response.status)
-          console.log(error.response.headers)
+          console.log('data: ', data)
+          console.log('status: ', status)
+          console.log('headers: ', error.response.headers)
 
-          if (
-            error.response.status == HttpStatus.CONFLICT
-          ) {
-            NotificationManager.error(
-              'Email already used!',
-              'Error',
-            )
-          }
+          NotificationManager.error(
+            `${status}:${data.message}`,
+            'Error',
+          )
         } else if (error.request) {
+          // ERROR: SERVER NO RESPONSE
+          console.log('ERROR: SERVER NO RESPONSE')
           // The request was made but no response was received
           // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
           // http.ClientRequest in node.js
           console.log(error.request)
         } else {
+          // ERROR: REQUEST ERROR
+          console.log('ERROR: REQUEST ERROR')
+
           // Something happened in setting up the request that triggered an Error
+
           console.log('Error', error.message)
         }
+        //ERROR: CONFIG
+        console.log('ERROR: CONFIG')
         console.log(error.config)
       })
   }

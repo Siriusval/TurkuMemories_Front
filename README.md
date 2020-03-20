d4b65784cae1541f2d4fb21900c2539dc28a3973
Siriusval <siriusval@hotmail.fr>
2020-03-20 20:38:03 +0200

    Fixed build and prod scripts

--- README.md
+++ README.md
@@ -20,9 +20,9 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
-### `npm run tsc`
+### `npm run build`
 
-Builds the app for production to the `build` folder.<br />
+Builds the app for production to the `.nextjs` folder.<br />
 It correctly bundles NextJS in production mode and optimizes the build for the best performance.
 Also transpile the Typescript into Javascript
 The build is minified and the filenames include the hashes.<br />
--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,1 +1,25 @@
-export default {};
+import React from 'react';
+import CustomAppBar from '../components/CustomAppBar';
+import { withTranslation } from '../i18n';
+import { Typography } from '@material-ui/core';
+import { Layout } from '../components/Layout';
+
+const Admin = ({ t }) => {
+    return (
+        <div>
+            <CustomAppBar />
+            <Layout>
+                <Typography variant="h3">Admin</Typography>
+                <div style={{ height: '5vh' }} />
+            </Layout>
+        </div>
+    );
+};
+
+Admin.getInitialProps = async ({ req }) => {
+    return {
+        namespacesRequired: ['common'],
+    };
+};
+
+export default withTranslation('common')(Admin as any);

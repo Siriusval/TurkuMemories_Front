d69db95af405711b7b6b1cfc240ac21f7ec213bb
Antti Rae <antti.rae@edu.turkuamk.fi>
2020-04-14 10:44:56 +0300

    rename files and change page refences to match new names

--- src/components/CustomAppBar.tsx
+++ src/components/CustomAppBar.tsx
@@ -82,7 +82,7 @@ const CustomAppBar: React.FC<ICustomAppBar> = ({ t, isLogged }) => {
 
                 {/* Add memory */}
 
-                <Link href="/addMemory" passHref>
+                <Link href="/add_memory" passHref>
                     <Button
                         component="a"
                         variant="contained"
